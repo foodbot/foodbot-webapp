9a9f409a232fd36b3a7c490fa666236ad16e7bba
Rob Graeber <bgraeber@gmail.com>
2014-04-03 12:36:24 -0700

    Initial Commit, changed port to 3050

---
+++ excludedPhrases.js
@@ -0,0 +1,9 @@
+exports.regexpList = [
+  /(\W|^)bars?(\W|$)/i,
+  /(\W|^)restaurant(\W|$)/i,
+  /(\W|^)buy(\W|$)/i,
+  /(\W|^)bring food(\W|$)/i,
+  /(\W|^)brewery(\W|$)/i,
+  /(\W|^)cost(\W|$)/i,
+  /(\W|^)food bank(\W|$)/i,
+];
