d1ca16e6c492788f40805d89bd17be60d638130d
Rob Graeber <bgraeber@gmail.com>
2014-06-08 22:24:53 -0700

    Code cleanup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var routes = require('./routes/index');
 var test = require('./routes/test');
-
+var path = require('path');
 var app = module.exports = express();
 
 /**
@@ -13,7 +13,9 @@ var app = module.exports = express();
  */
 
 app.set('port', process.env.PORT || 3050);
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 /**
  * Routes
  */
