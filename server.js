19c89b93770ec91b796717a434106ec677c54526
Rob Graeber <bgraeber@gmail.com>
2014-08-05 01:25:11 -0700

    Removed www redirect

--- server.js
+++ server.js
@@ -8,19 +8,10 @@ var test = require('./routes/test');
 var path = require('path');
 var app = module.exports = express();
 
- //redirects to www domain except for subdomains
-var wwwRedirect = function (req, res, next) {
-  if (req.headers.host.slice(0, 3) !== 'www' && !req.headers.host.match(/^(localhost|.*?\..*?\.)/)) {
-    res.redirect(req.protocol + '://www.' + req.headers.host + req.url);
-  }else{
-    next();
-  }
-};
 /**
  * Configuration
  */
 app.set('port', process.env.PORT || 8000);
-app.use(wwwRedirect);
 app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public')));
 /**
